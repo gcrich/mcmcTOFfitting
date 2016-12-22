b061ae66593f8be79d1055319badc90692097fa8
Marcus Medley <mdmeds@gmail.com>
2016-12-21 23:04:06 -0600

    Example of pythonic constants
    Created constants directory. Added constants module for
    tof constants. Added example usage module to demostrate usage of
    the new constants' pattern.

---
+++ tests/example_usage.py
@@ -0,0 +1,8 @@
+# They are imported into other modules like so...
+from constants.tof_constants import (Distances, Energy, Physics, Masses)
+
+# Then you acces them within their namespaces like so...
+print 'Cell length is: {0}'.format(Distances.distance_cellLength)
+print 'The Q value of a DDN reaction is: {0}'.format(Energy.qValue_ddn)
+print 'The speed of light is: {0}'.format(Physics.speedOfLight)
+print 'The mass of Deuteron is: {0}'.format(Masses.mass_deuteron)

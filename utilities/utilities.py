eedcce100643ea008af1cfeda29a5153b5f9fc70
grayson rich <grayson@tunl.duke.edu>
2016-12-25 00:58:50 -0500

    added some boundary catching in ddn xs interpolator AND handled single values and arrays

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -317,8 +317,14 @@ class ddnXSinterpolator:
         # check  to see if we have requested an energy outside of range
         # if so, return the XS value at the end of the range
         # this is not ideal, but we really ought not find these regions matter
-        if deuteronEnergy <= 20:
-            return 0.025
-        if deuteronEnergy >= 10000:
-            return 46.5
+        if len(deuteronEnergy)==1:
+            if deuteronEnergy <= 20:
+                return 0.025
+            if deuteronEnergy >= 10000:
+                return 46.5
+        else:
+            lowIndices = np.where(deuteronEnergy <= 20)
+            highIndices = np.where(deuteronEnergy >= 10000)
+            deuteronEnergy[lowIndices] = 20
+            deuteronEnergy[highIndices] = 10000
         return self.ddnXSfunc(deuteronEnergy)

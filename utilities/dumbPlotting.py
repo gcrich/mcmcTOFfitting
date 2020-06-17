09fd788cf9f65d372f1a3ed521b452f974fc9dd6
grayson <grayson@tunl.duke.edu>
2020-06-16 22:57:17 -0400

    added stupid plotting tool to visualize lognorm

---
+++ utilities/dumbPlotting.py
@@ -0,0 +1,56 @@
+#
+# dumbPlotting.py
+#
+# meant to help me visualize distributions
+
+from __future__ import print_function
+import numpy as np
+from numpy import inf
+import scipy.optimize as optimize
+from scipy.integrate import ode
+from scipy.interpolate import RectBivariateSpline
+from scipy.stats import (poisson, norm, lognorm)
+import scipy.special as special
+#from scipy.stats import skewnorm
+import sys
+import emcee
+import csv as csvlib
+import argparse
+from numbers import Number
+from math import isnan
+import gc
+import matplotlib.pyplot as plt
+
+#nBins = 600
+binWidth = 50
+range_min, range_max = (0, 3000)
+
+bins = np.arange(range_min, range_max, binWidth)
+nBins = len(bins)
+binCenters = np.linspace( range_min + binWidth/2, range_max - binWidth/2, nBins, True)
+
+beamE = 2400.
+par1, par2, par3 = (800, 150, 0.5)
+
+nEvs = 20000
+eZeros = np.repeat(beamE, nEvs)
+eZeros -= lognorm.rvs(s=par3, loc=par1, scale=par2, size=nEvs)
+
+par1, par2, par3 = (800, 300, 0.5)
+eZeros_1 = np.repeat(beamE, nEvs)
+eZeros_1 -= lognorm.rvs(s=par3, loc=par1, scale=par2, size=nEvs)
+
+par1, par2, par3 = (800, 150, 1.)
+eZeros_2 = np.repeat(beamE, nEvs)
+eZeros_2 -= lognorm.rvs(s=par3, loc=par1, scale=par2, size=nEvs)
+
+hist, edges = np.histogram(eZeros, len(bins), (range_min, range_max))
+hist2, edges = np.histogram(eZeros_1, len(bins), (range_min, range_max))
+hist_2, edges = np.histogram(eZeros_2, len(bins), (range_min, range_max))
+
+fig, ax = plt.subplots(figsize=(8.5,5.25))
+ax.scatter( binCenters, hist, color='k' )
+ax.scatter( binCenters, hist2, color='red')
+ax.scatter( binCenters, hist_2, color='blue')
+
+plt.draw()
